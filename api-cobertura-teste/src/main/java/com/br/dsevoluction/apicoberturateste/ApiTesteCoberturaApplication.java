f6f567ce761796675eb7dd999687dedc4fcfe3c5
Douglass28 <douglas.souza1997@hotmail.com>
2022-11-30 20:57:47 -0300

    Projeto Criado

---
+++ api-cobertura-teste/src/main/java/com/br/dsevoluction/apicoberturateste/ApiTesteCoberturaApplication.java
@@ -0,0 +1,13 @@
+package com.br.dsevoluction.apicoberturateste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTesteCoberturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTesteCoberturaApplication.class, args);
+	}
+
+}
---
+++ api-cobertura-teste/src/test/java/com/br/dsevoluction/apicoberturateste/ApiTesteCoberturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.dsevoluction.apicoberturateste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTesteCoberturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
