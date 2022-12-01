3ce56ad9d223672ffe12eceb5c27b0fe907246f8
Douglass28 <douglas.souza1997@hotmail.com>
2022-11-30 21:18:11 -0300

    Entidade User

---
+++ api-cobertura-teste/src/main/java/com/br/dsevoluction/apicoberturateste/entities/User.java
@@ -0,0 +1,24 @@
+package com.br.dsevoluction.apicoberturateste.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
